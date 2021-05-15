1e8510c6a87517b36df2157d696bfc2b2128f3fd
Charlie <38168227+theycallmecharlie@users.noreply.github.com>
2021-05-15 08:51:05 -0300

    ,

--- pypass.py
+++ pypass.py
@@ -2,17 +2,8 @@
 import sys
 
 from src.Generator import *
-
-def start():
-    start = '''
-Password Length
-Weak    | 0 - 15
-Strong  | 16 + 
-        '''
-    return start
 def main():
     try:
-        print(start())
         if len(sys.argv) > 1:
             length = int(sys.argv[1])
             pw = Generator(length)
