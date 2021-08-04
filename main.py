ca0da6061fbc5e417d83828010a7e4c762968588
theycallmecharlie <38168227+theycallmecharlie@users.noreply.github.com>
2021-08-03 21:44:30 -0300

    .

---
+++ main.py
@@ -0,0 +1,20 @@
+#encoding=utf8
+#!/usr/bin/python3
+import argparse
+import sys
+import string
+
+from src.Gen import *
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Simple secure password generator')
+    #parser.add_argument('-p', '--password', help = 'Generate a password', action = 'store_true')
+    parser.add_argument('-l', '--length', help = 'Define password length, if not flagged, the default length is 8 chars', type = int, default = 8)
+    #parser.add_argument('-s', '--symbols', help = 'Exclude special characters to password', action = "store_true", default = False)
+    #parser.add_argument('-n', '--numbers', help = 'Exclude numbers to password', action = "store_true", default = False)
+    #parser.add_argument('-v', '--verbose', help = 'It provides additional details', action = "store_true", default= False)
+    args = parser.parse_args()  
+    gen = Gen(args.length)
+    gen.newpw()
+if __name__=='__main__':
+    main()
