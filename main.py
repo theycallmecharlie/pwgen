3b7119a72ad7de1872a0bfb310d2765f087100b2
theycallmecharlie <38168227+theycallmecharlie@users.noreply.github.com>
2021-12-02 23:44:26 -0300

    -

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.Passphrase import *
 def main():
     parser = argparse.ArgumentParser(description = "Simple secure password generator")
     parser.add_argument('-pw', '--password', help = 'Generate a password', action = 'store_true')
-    parser.add_argument('-pp', '--passphrase', help = 'Generate a passphrase w/ 12 words', action = 'store_true')
+    parser.add_argument('-pp', '--passphrase', help = 'Generate a passphrase w/ N words, the default is 8 words', action = 'store_true')
     parser.add_argument('-l', '--length', help = 'Define password length, if not flagged, the default length is 8 chars', type = int, default = 8)
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
@@ -15,7 +15,7 @@ def main():
         gen = Password(args.length,args.verbose)
         gen.newpw()
     if(args.passphrase): 
-        gen = Passphrase(12,args.verbose)
+        gen = Passphrase(args.length,args.verbose)
         gen.passphrase()
     else: parser.print_help()
 if __name__=='__main__':
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pypwgen',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/theycallmecharlie',
+    license='',
+    author='Charlie',
+    author_email='',
+    description='Password/Passphrase generator'
+)
